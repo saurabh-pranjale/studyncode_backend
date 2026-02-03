48d0736ec1d73f0556dea905bc457f55a2b1b66c
Saurabh Pranjale <saurabh.pranjale20@gmail.com>
2026-02-03 20:16:06 +0530

    [feat]:authentication

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // Importing necessary modules and packages
 const express = require("express");
 const app = express();
-// const userRoutes = require("./routes/user");
+const userRoutes = require("./routes/user.route.js");
 // const profileRoutes = require("./routes/profile");
 // const courseRoutes = require("./routes/Course");
 // const paymentRoutes = require("./routes/Payments");
 // const contactUsRoute = require("./routes/Contact");
 const database = require("./config/database");
-// const cookieParser = require("cookie-parser");
-// const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 // const { cloudinaryConnect } = require("./config/cloudinary");
 // const fileUpload = require("express-fileupload");
 const dotenv = require("dotenv");
@@ -23,14 +23,14 @@ dotenv.config();
 database.connect();
  
 // Middlewares
-// app.use(express.json());
-// app.use(cookieParser());
-// app.use(
-// 	cors({
-// 		origin: "*",
-// 		credentials: true,
-// 	})
-// );
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+	cors({
+		origin: "*",
+		credentials: true,
+	})
+);
 // app.use(
 // 	fileUpload({
 // 		useTempFiles: true,
@@ -42,7 +42,7 @@ database.connect();
 // cloudinaryConnect();
 
 // Setting up routes
-// app.use("/api/v1/auth", userRoutes);
+app.use("/api/v1/auth", userRoutes);
 // app.use("/api/v1/profile", profileRoutes);
 // app.use("/api/v1/course", courseRoutes);
 // app.use("/api/v1/payment", paymentRoutes);
